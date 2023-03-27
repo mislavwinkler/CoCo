7f708db0c144ba43449308d0770c410529bb1037
Jakov <jakov.mucnjak@gmail.com>
2023-03-27 16:11:02 +0200

    Fixed DI issues

--- app/src/main/java/com/diplomski/mucnjak/coco/domain/mapper/lesson_mapper/LessonMapper.kt
+++ app/src/main/java/com/diplomski/mucnjak/coco/domain/mapper/lesson_mapper/LessonMapper.kt
@@ -2,8 +2,9 @@ package com.diplomski.mucnjak.coco.domain.mapper.lesson_mapper
 
 import com.diplomski.mucnjak.coco.data.remote.response.LessonResponse
 import com.diplomski.mucnjak.coco.data.ui.Lesson
+import javax.inject.Inject
 
-class LessonMapper : LessonMappers.LessonMapper {
+class LessonMapper @Inject constructor(): LessonMappers.LessonMapper {
 
     override fun mapToUiModel(networkModel: LessonResponse) =
         Lesson(
--- app/src/main/java/com/diplomski/mucnjak/coco/domain/storages/StudentStorage.kt
+++ app/src/main/java/com/diplomski/mucnjak/coco/domain/storages/StudentStorage.kt
@@ -1,7 +1,8 @@
 package com.diplomski.mucnjak.coco.domain.storages
 
 import com.diplomski.mucnjak.coco.data.ui.Student
+import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class StudentStorage : ListStorage<Student>()
+class StudentStorage @Inject constructor() : ListStorage<Student>()
